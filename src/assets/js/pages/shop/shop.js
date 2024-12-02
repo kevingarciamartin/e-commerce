ce0b184a36234f2731f95aa0f282ca0fc33ffef6
Kevin Garcia Martin <kevingm@live.se>
2024-12-02 17:23:41 +0100

    Render page heading

--- src/assets/js/pages/shop/shop.js
+++ src/assets/js/pages/shop/shop.js
@@ -1,8 +1,10 @@
 import "./shop.css";
 import { resetMain } from "../../utils/helpers.js";
+import { renderPageHeading } from "../../components/pageHeading/pageHeading.js";
 import { renderProducts } from "../../components/products/products.js";
 
 export function renderShop() {
   resetMain();
+  renderPageHeading('Our products');
   renderProducts();
 }

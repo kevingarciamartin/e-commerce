38770228f2fd9b68778e9541e4195ab9a3e383c9
noory007 <satar.noory@hyperisland.se>
2024-12-03 14:51:05 +0100

    I have made two folder under home folder by the name of featuredproducts and graphics and also I made js and css files for them.

---
+++ src/assets/js/pages/home/graphics/graphics.js
@@ -0,0 +1,7 @@
+import "./graphics.css";
+import graphics from "../../../../images/Graphics.png";
+//import { renderShop } from "./src/assets/js/pages/shop/shop.js";
+
+export function rendergraphics() {
+  const main = document.querySelector("main");
+}
--- src/assets/js/pages/home/home.js
+++ src/assets/js/pages/home/home.js
@@ -1,8 +1,10 @@
 import "./home.css";
 import { resetMain } from "../../utils/helpers";
 import { renderHero } from "./hero/hero";
+import { rendergraphics } from "./graphics/graphics";
 
 export function renderHome() {
   resetMain();
   renderHero();
+  rendergraphics();
 }

15e8213381aaf8a62c6759dfe68f82fe1b2cba9f
noory007 <satar.noory@hyperisland.se>
2024-12-04 11:03:54 +0100

    I made these folders and files

--- src/assets/js/pages/home/home.js
+++ src/assets/js/pages/home/home.js
@@ -1,10 +1,10 @@
 import "./home.css";
 import { resetMain } from "../../utils/helpers";
 import { renderHero } from "./hero/hero";
-import { rendergraphics } from "./graphics/graphics";
+import { renderGraphics } from "./graphics/graphics";
 
 export function renderHome() {
   resetMain();
   renderHero();
-  rendergraphics();
+  renderGraphics();
 }

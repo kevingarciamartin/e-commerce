5df8120fa669669ce51fba604cb62aa7ca5c987d
Kevin Garcia Martin <kevingm@live.se>
2024-12-09 11:52:25 +0100

    Fix searchbar results visibility

--- src/assets/js/components/searchbar/searchbar.js
+++ src/assets/js/components/searchbar/searchbar.js
@@ -1,4 +1,4 @@
-import './searchbar.css'
+import "./searchbar.css";
 
 export function renderSearchbar() {
   const searchBarWrapper = document.getElementById("search-bar-wrapper");
@@ -37,14 +37,16 @@ export function renderSearchbar() {
   fetch("https://fakestoreapi.com/products")
     .then((response) => response.json())
     .then((data) => {
-      products = data; 
+      products = data;
     })
     .catch((error) => {
       console.error("Error fetching products:", error);
     });
 
-  // Event Listener 
+  // Event Listener
   searchButton.addEventListener("click", () => {
+    resultsContainer.style.display = "block";
+
     const searchValue = searchInput.value.trim().toLowerCase();
     const results = products.filter((product) =>
       product.title.toLowerCase().includes(searchValue)
@@ -59,6 +61,8 @@ export function renderSearchbar() {
 
     if (!isClickInsideSearchbar) {
       resultsContainer.innerHTML = ""; // Clear search results
+      resultsContainer.style.display = "none";
+      searchInput.value = "";
     }
   });
 }
